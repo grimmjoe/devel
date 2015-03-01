c0babd73c9a19676618ede9aa98200efdc8cc650
Joe <grimm.joe@outlook.com>
2015-03-02 00:26:15 +0400

    Added the base class for function objects

---
+++ core/inc/function.h
@@ -0,0 +1,20 @@
+#ifndef __FUNCTION_H__
+#define __FUNCTION_H__
+
+#include <string>
+#include <memory>
+
+namespace core
+{
+	template <class T>
+	class function
+	{
+	public:
+		virtual T operator()(const T& t) = 0;
+		virtual std::string toString() const = 0;
+		virtual std::shared_ptr<function<T> > derivative(int num) = 0;
+		virtual void optimize() = 0;
+	};
+}
+
+#endif // __FUNCTION_H__
