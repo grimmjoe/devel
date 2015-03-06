f387f5109b80b546ded28191f6aa6136e878e97d
Joe <grimm.joe@outlook.com>
2015-03-06 21:49:55 +0400

    Added a method to apply single-point differential transformations in the apparatus

--- core/inc/apparatus.h
+++ core/inc/apparatus.h
@@ -4,35 +4,77 @@
 #include "function.h"
 #include "matrix.h"
 #include <vector>
+#include <cmath>
 
 namespace core
 {
-	namespace app
+	template <class T>
+	class apparatus
 	{
-		template <class T>
-		class apparatus
+	public:
+		typedef T tArgType;
+		typedef typename function<T>::tFunction tFunction;
+		typedef typename function<T>::tFunctionPtr tFunctionPtr;
+		typedef core::matrix<tFunctionPtr> tFuncMatrix;
+		typedef core::matrix<T> tMatrixDiscrete;
+		typedef std::vector<tMatrixDiscrete> tMatrixDiscretes;
+	
+		struct diffInfo
 		{
-		public:
-			typedef T tArgType;
-			typedef typename function<T>::tFunction tFunction;
-			typedef typename function<T>::tFunctionPtr tFunctionPtr;
-			typedef core::matrix<tFunctionPtr> tFuncMatrix;
-			typedef core::matrix<T> tDiscrete;
-			typedef std::vector<tDiscrete> tDiscretes;
-
-			//
-			/// Get LU decomposition of the matrix
-			//
-			bool getLU(const tFuncMatrix& sourceMatrix, int K, tDiscrete& l, tDiscrete& u);
+			T tv;
+			T H;
+			int K;
+			diffInfo(T t, T h, T k)
+				: tv(t)
+				, H(h)
+				, K(k)
+			{}
 		};
-	} // namespace app
+	
+		typedef diffInfo tDiffInfo;
+	
+		//
+		/// Apply the Taylor-diff transformation @a di on the @a sourceMatrix into the discretes @a discs
+		//
+		bool applyDiffTrans(const tFuncMatrix& sourceMatrix, const tDiffInfo& di, tMatrixDiscretes& discs);
+	
+		//
+		/// Get LU decomposition of the matrix
+		//
+		bool getLU(const tFuncMatrix& sourceMatrix, const diffInfo& di, tMatrixDiscretes& l, tMatrixDiscretes& u);
+	};
 } // namespace core
 
 template <class T>
-bool core::apparatus<T>::getLU(const tFuncMatrix& sourceMatrix, int K, tDiscrete& l, tDiscrete& u)
+bool core::apparatus<T>::getLU(const tFuncMatrix& sourceMatrix, const diffInfo& di, tMatrixDiscretes& l, tMatrixDiscretes& u)
 {
 	const int m = sourceMatrix.getNumRows();
 	const int n = sourceMatrix.getNumCols();
+	return true;
 }
+
+template <class T>
+bool core::apparatus<T>::applyDiffTrans(const tFuncMatrix& sourceMatrix, const tDiffInfo& di, tMatrixDiscretes& discs)
+{
+	const int m = sourceMatrix.getNumRows();
+	const int n = sourceMatrix.getNumCols();
+	discs.resize(di.K+1, tMatrixDiscrete(m, n, 0));
+	tFuncMatrix theMatrix = sourceMatrix;
+	long long kfac = 1;
+	for (int k = 0; k <= di.K; ++k)
+	{
+		kfac *= (k > 0 ? k : 1);
+		for (int i = 1; i <= m; ++i)
+		{
+			for (int j = 1; j <= n; ++j)
+			{
+				theMatrix[i][j] = theMatrix[i][j]->derivative(k > 0 ? 1 : 0);
+				discs[k][i][j] = std::pow(di.H, k)*(*theMatrix[i][j])(di.tv)/(T)kfac;
+			}
+		}
+	}
+	return true;
+}
+
 
 #endif // __APPARATUS_H__
--- test/test_core/src/deriv_matrix.cpp
+++ test/test_core/src/deriv_matrix.cpp
@@ -3,15 +3,20 @@
 #include "function.h"
 #include "matrix.h"
 #include "parser.h"
+#include "apparatus.h"
 #include <iostream>
 #include <fstream>
 #include <cassert>
 #include <vector>
 #include <iterator>
 
-typedef typename core::function<double>::tFunction tFunction;
-typedef typename core::function<double>::tFunctionPtr tFunctionPtr;
-typedef core::matrix<tFunctionPtr> tMatrix;
+typedef core::apparatus<double> tApp;
+typedef typename tApp::tMatrixDiscrete tDiscrete;
+typedef typename tApp::tMatrixDiscretes tDiscretes;
+typedef typename tApp::tFunction tFunction;
+typedef typename tApp::tFunctionPtr tFunctionPtr;
+typedef typename tApp::tFuncMatrix tMatrix;
+typedef typename tApp::tDiffInfo tDiffInfo;
 
 void printFuncMatrix(const tMatrix& mat)
 {
@@ -52,29 +57,33 @@ int main(int argc, char* argv[])
 	}
 	std::cout << "The matrix:\n";
 	printFuncMatrix(theMatrix);
-	std::cout << "Calculating the A^3...\n";
-	tMatrix theM = theMatrix * theMatrix;
-	//std::cout << "A^2:\n";
-	//theMatrix = theM;
+	//std::cout << "Calculating the A^3...\n";
+	//tMatrix theM = theMatrix * theMatrix;
+	////std::cout << "A^2:\n";
+	////theMatrix = theM;
+	////printFuncMatrix(theMatrix);
+	//theMatrix = theM * theMatrix;
+	//std::cout << "A^3:\n";
 	//printFuncMatrix(theMatrix);
-	theMatrix = theM * theMatrix;
-	std::cout << "A^3:\n";
-	printFuncMatrix(theMatrix);
-	typedef core::matrix<double> tDiscrete;
-	std::vector<tDiscrete> theDiscretes(k+1, tDiscrete(m, n, 0));
-	for (int K = 0; K <= k; ++K)
-	{
-		for (int i = 1; i <= m; ++i)
-		{
-			for (int j = 1; j <= n; ++j)
-			{
-				theMatrix[i][j] = theMatrix[i][j]->derivative(K > 0 ? 1 : 0);
-				theDiscretes[K][i][j] = (*theMatrix[i][j])(tv);
-			}
-		}
-		std::cout << "K: = " << K << std::endl;
-		printFuncMatrix(theMatrix);
-	}
+	//typedef core::matrix<double> tDiscrete;
+	//std::vector<tDiscrete> theDiscretes(k+1, tDiscrete(m, n, 0));
+	tDiffInfo di(tv, 1, k);
+	tDiscretes theDiscretes;
+	tApp app;
+	app.applyDiffTrans(theMatrix, di, theDiscretes);
+	//for (int K = 0; K <= k; ++K)
+	//{
+	//	for (int i = 1; i <= m; ++i)
+	//	{
+	//		for (int j = 1; j <= n; ++j)
+	//		{
+	//			theMatrix[i][j] = theMatrix[i][j]->derivative(K > 0 ? 1 : 0);
+	//			theDiscretes[K][i][j] = (*theMatrix[i][j])(tv);
+	//		}
+	//	}
+	//	std::cout << "K: = " << K << std::endl;
+	//	printFuncMatrix(theMatrix);
+	//}
 	std::cout << "\nDiscretes:\n";
 	std::copy(theDiscretes.begin(), theDiscretes.end(), std::ostream_iterator<tDiscrete>(std::cout, "\n"));
 
