21bb46654e097a853da8eb8a1b58338e7d6ee51c
Joe <grimm.joe@outlook.com>
2015-03-09 18:29:20 +0400

    Matrix inverse

--- core/inc/apparatus.h
+++ core/inc/apparatus.h
@@ -11,11 +11,35 @@
 #include <vector>
 #include <cmath>
 
+//#include <iostream>
+//#include <algorithm>
+//#include <iterator>
+
 #define EPSILON_FOR_ZERO 0.00000001
 
 namespace core
 {
-	template <class T, bool isParallel = false>
+	enum eAlgoType
+	{
+		eAlgoBasic = 0,
+		eAlgoParallel = 1
+	};
+
+
+	class algoException : public std::exception
+	{
+		std::string mErr;
+	public:
+		algoException(const std::string& err)
+			: mErr(err)
+		{}
+		const char* what() const noexcept
+		{
+			return mErr.c_str();
+		}
+	};
+
+	template <class T, int algo = eAlgoBasic>
 	class apparatus
 	{
 	public:
@@ -90,21 +114,402 @@ namespace core
 		/// Get LU decomposition of the matrix
 		//
 		bool getLU(const tFuncMatrix& sourceMatrix, tMatrixDiscretes& l, tMatrixDiscretes& u);
+
+		//
+		/// Get LU decomposition of the matrix
+		//
+		bool getLU(const tFuncMatrix& sourceMatrix, int r, tMatrixDiscretes& l, tMatrixDiscretes& u);
+
+		//
+		/// Get LU decomposition of the matrix discretes
+		//
+		bool getLU(const tMatrixDiscretes& discretes, int r, tMatrixDiscretes& L, tMatrixDiscretes& U);
+
+		//
+		/// Get the rank of the matrix
+		//
+		int getRank(const tFuncMatrix& theMatrix) const;
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverse(const tFuncMatrix& theMatrix, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverse(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverse(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseLowerTriangular(const tFuncMatrix& theMatrix, tMatrixDiscretes& inv)
+		{
+			return this->getInverseLowerTriangular(theMatrix, getRank(theMatrix), inv);
+		}
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseLowerTriangular(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseLowerTriangular(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseUpperTriangular(const tFuncMatrix& theMatrix, tMatrixDiscretes& inv)
+		{
+			return this->getInverseUpperTriangular(theMatrix, getRank(theMatrix), inv);
+		}
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseUpperTriangular(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Get inverse of the matrix
+		//
+		bool getInverseUpperTriangular(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv);
+
+		//
+		/// Check if the matrix is upper triangular or not
+		//
+		bool isUpperTriangular(const tFuncMatrix& theMatrix) const;
+
+		//
+		/// Check if the matrix is lower triangular or not
+		//
+		bool isLowerTriangular(const tFuncMatrix& theMatrix) const;
+
+		//
+		/// Check if the matrix is triangular or not
+		//
+		bool isTriangular(const tFuncMatrix& theMatrix) const
+		{
+			return this->isLowerTriangular(theMatrix) || this->isUpperTriangular(theMatrix);
+		}
+
+		//
+		/// Check if the matrix is invertible or not
+		//
+		bool isInvertible(const tFuncMatrix& theMatrix) const
+		{
+			return this->isInvertible(theMatrix, this->getRank(theMatrix));
+		}
+
+		//
+		/// Check if the matrix is invertible or not
+		//
+		bool isInvertible(const tFuncMatrix& theMatrix, int r) const;
+
+		//
+		/// Check if the matrix is invertible or not
+		//
+		bool isInvertible(const tMatrixDiscretes& theMatrix, int r) const;
+
+		//
+		/// Check if the matrix is square or not
+		//
+		bool isSquare(const tFuncMatrix& theMatrix) const
+		{
+			return theMatrix.getNumRows() == theMatrix.getNumCols();
+		}
+
+		//
+		/// Check if the matrix is square or not
+		//
+		bool isSquare(const tMatrixDiscretes& discretes) const
+		{
+			assert (m_di.K >= 0);
+			return discretes[0].getNumRows() == discretes[0].getNumCols();
+		}
+
+		//
+		///
+		//
+	protected:
+		enum eTriangularType
+		{
+			eLowerTriangular = 0,
+			eUpperTriangular = 1,
+			eNoneTriangular = 2
+		};
+		bool impl_getInverse(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv, eTriangularType et);
+		T impl_identity(int k, int i, int j) const
+		{
+			return (k == 0 && i == j) ? 1 : 0;
+		}
 	private:
 		tDiffInfo m_di;
 	};
 } // namespace core
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::getLU(const tFuncMatrix& sourceMatrix, tMatrixDiscretes& l, tMatrixDiscretes& u)
+template <class T, int algo>
+int core::apparatus<T, algo>::getRank(const tFuncMatrix& theMatrix) const
 {
-	const int m = sourceMatrix.getNumRows();
-	const int n = sourceMatrix.getNumCols();
+	// TODO - We need to calculate the rank correctly!!!
+	return theMatrix.getNumRows();
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverseLowerTriangular(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv)
+{
+	if (!this->isInvertible(theMatrix, r))
+		throw algoException("Inversion: the matrix is not invertible");
+	tMatrixDiscretes discretes;
+	this->applyDiffTrans(theMatrix, discretes);
+	return this->getInverseLowerTriangular(discretes, r, inv);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::isInvertible(const tFuncMatrix& theMatrix, int r) const
+{
+	if (!isSquare(theMatrix))
+		return false;
+	if (r != theMatrix.getNumRows())
+		return false;
 	return true;
 }
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::applyDiffTrans(const tFuncMatrix& sourceMatrix, tMatrixDiscretes& discs)
+template <class T, int algo>
+bool core::apparatus<T, algo>::isInvertible(const tMatrixDiscretes& theMatrix, int r) const
+{
+	if (!isSquare(theMatrix))
+		return false;
+	assert (m_di.K >= 0);
+	if (r != theMatrix[0].getNumRows())
+		return false;
+	return true;
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverseLowerTriangular(const tMatrixDiscretes& L, int r, tMatrixDiscretes& L1)
+{
+	if (!this->isInvertible(L, r))
+		throw algoException("Inversion: the matrix is not invertible");
+	assert (m_di.K >= 0);
+	const int n = L[0].getNumRows();
+	L1.resize(m_di.K+1, tMatrixDiscrete(n, r, 0));
+	
+	int K = m_di.K;
+	// Now L1
+	for (int k = 0; k <= K; ++k)
+	{
+		for (int i = 1; i <= r; ++i)
+		{
+			for (int j = 1; j <= r; ++j)
+			{
+				double sum1 = 0;
+				for (int p = 1; p <= i-1; ++p)
+					for (int l = 0; l <= k; ++l)
+						sum1 += L[l][i][p]*L1[k-l][p][j];
+
+				double sum2 = 0;
+				for (int l = 1; l <= k; ++l)
+					sum2 += L1[k-l][i][j]*L[l][i][i];
+				L1[k][i][j] = (impl_identity(k, i, j) - sum1 - sum2)/L[0][i][i];
+			}
+		}
+	}
+	return true;
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverseUpperTriangular(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv)
+{
+	if (!this->isInvertible(theMatrix, r))
+		throw algoException("Inversion: the matrix is not invertible");
+	tMatrixDiscretes discretes;
+	this->applyDiffTrans(theMatrix, discretes);
+	return this->getInverseUpperTriangular(discretes, r, inv);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverseUpperTriangular(const tMatrixDiscretes& U, int r, tMatrixDiscretes& U1)
+{
+	if (!this->isInvertible(U, r))
+		throw algoException("Inversion: the matrix is not invertible");
+	assert (m_di.K >= 0);
+	const int m = U[0].getNumCols();
+	int K = m_di.K;
+	U1.resize(K+1, tMatrixDiscrete(r, m, 0));
+	// Now U1
+	for (int k = 0; k <= K; ++k)
+	{
+		for (int i = r; i >= 1; --i)
+		{
+			for (int j = 1; j <= r; ++j)
+			{
+				double sum = 0;
+				for (int p = i+1; p <= m; ++p)
+					for (int g = 0; g <= k; ++g)
+						sum += U[g][i][p]*U1[k-g][p][j];
+				U1[k][i][j] = impl_identity(k, i, j) - sum;
+			}
+		}
+	}
+	return true;
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::impl_getInverse(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv, eTriangularType et)
+{
+	switch (et)
+	{
+	case eLowerTriangular:
+		return this->getInverseLowerTriangular(theMatrix, r, inv);
+	break;
+	case eUpperTriangular:
+		return this->getInverseUpperTriangular(theMatrix, r, inv);
+	break;
+	default:
+	break;
+	}
+	tMatrixDiscretes L;
+	tMatrixDiscretes U;
+	this->getLU(theMatrix, r, L, U);
+	tMatrixDiscretes L1;
+	this->getInverseLowerTriangular(L, r, L1);
+	tMatrixDiscretes U1;
+	this->getInverseUpperTriangular(U, r, U1);
+	//std::cout << "L1:\n";
+	//std::copy(L1.begin(), L1.end(), std::ostream_iterator<tMatrixDiscrete>(std::cout, "\n"));
+	//std::cout << "U1:\n";
+	//std::copy(U1.begin(), U1.end(), std::ostream_iterator<tMatrixDiscrete>(std::cout, "\n"));
+	return this->multDiscretes(U1, L1, inv);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverse(const tMatrixDiscretes& theMatrix, int r, tMatrixDiscretes& inv)
+{
+	return this->impl_getInverse(theMatrix, r, inv, eNoneTriangular);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverse(const tFuncMatrix& theMatrix, tMatrixDiscretes& inv)
+{
+	return this->getInverse(theMatrix, getRank(theMatrix), inv);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getInverse(const tFuncMatrix& theMatrix, int r, tMatrixDiscretes& inv)
+{
+	if (!this->isInvertible(theMatrix, r))
+		throw algoException("Inversion: the matrix is not invertible");
+	tMatrixDiscretes discretes;
+	this->applyDiffTrans(theMatrix, discretes);
+	return this->getInverse(discretes, r, inv);
+
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getLU(const tMatrixDiscretes& discretes, int r, tMatrixDiscretes& L, tMatrixDiscretes& U)
+{
+	int K = m_di.K;
+	assert (K >= 0);
+	const int m = discretes[0].getNumRows();
+	const int n = discretes[0].getNumCols();
+	if (m != n)
+		throw matrixException("Matrix is not square!"); 
+	
+	if (is_equal(discretes[0][1][1], 0))
+		throw algoException("LU: The element 1,1 is 0!");
+	L.resize(m_di.K+1, tMatrixDiscrete(m, n, 0));
+	U.resize(m_di.K+1, tMatrixDiscrete(m, n, 0));
+
+
+	// First
+	for (int k = 0; k < K; ++k)
+		for (int i = 1; i <= r; ++i)
+			L[k][i][1] = discretes[k][i][1];
+	
+	for (int k = 0; k < K; ++k)
+	{
+		for (int j = 1; j <= r ; ++j)
+		{
+			double sum = 0;
+			for (int l = 1; l <= k; ++l)
+			{
+				sum += U[k-l][1][j]*L[l][1][1];
+			}
+			U[k][1][j] = (discretes[k][1][j]-sum)/L[0][1][1];
+		}
+	}
+
+	// Next
+	for (int k = 0; k < K; ++k)
+	{
+		for (int i = 2; i <= r; ++i)
+		{
+			for (int p = 2; p <= i; ++p)
+			{
+				double sum = 0;
+				for (int j = 1; j <= p-1; ++j)
+				{
+					for (int l = 0; l <= k; ++l)
+					{
+						sum += L[l][i][j]*U[k-l][j][p];
+					}
+				}
+				L[k][i][p] = discretes[k][i][p] - sum;
+			}
+		}
+	}
+
+	for (int k = 0; k < K; ++k)
+	{
+		for (int i = 2; i <= r; ++i)
+		{
+			for (int p = i; p <= r; ++p)
+			{
+				double sum1 = 0;
+				for (int j = 1; j <= i-1; ++j)
+					for (int g = 0; g <= k; ++g)
+						sum1 += L[g][i][j]*U[k-g][j][p];
+
+				double sum2 = 0;
+				for (int l = 1; l <= k; ++l)
+				{
+					sum2 += U[k-l][i][p]*L[l][i][i];
+				}
+				U[k][i][p] = (discretes[k][i][p] - sum1 - sum2)/L[0][i][i];
+			}
+		}
+	}
+	
+	return true;
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getLU(const tFuncMatrix& sourceMatrix, int r, tMatrixDiscretes& L, tMatrixDiscretes& U)
+{
+	if (!this->isSquare(sourceMatrix))
+		throw matrixException("Matrix is not square!"); 
+	tMatrixDiscretes discretes;
+	this->applyDiffTrans(sourceMatrix, discretes);
+	return this->getLU(discretes, r, L, U);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::getLU(const tFuncMatrix& sourceMatrix, tMatrixDiscretes& L, tMatrixDiscretes& U)
+{
+	if (!this->isSquare(sourceMatrix))
+		throw matrixException("Matrix is not square!"); 
+	return this->getLU(sourceMatrix, this->getRank(sourceMatrix), L, U);
+}
+
+template <class T, int algo>
+bool core::apparatus<T, algo>::applyDiffTrans(const tFuncMatrix& sourceMatrix, tMatrixDiscretes& discs)
 {
 	const int m = sourceMatrix.getNumRows();
 	const int n = sourceMatrix.getNumCols();
@@ -126,8 +531,8 @@ bool core::apparatus<T, isParallel>::applyDiffTrans(const tFuncMatrix& sourceMat
 	return true;
 }
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::addDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
+template <class T, int algo>
+bool core::apparatus<T, algo>::addDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
 {
 	assert (m_di.K == x.size()+1);
 	assert (m_di.K == y.size()+1);
@@ -145,8 +550,8 @@ bool core::apparatus<T, isParallel>::addDiscretes(const tMatrixDiscretes& x, con
 	return true;
 }
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::subtractDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
+template <class T, int algo>
+bool core::apparatus<T, algo>::subtractDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
 {
 	assert (m_di.K == x.size()+1);
 	assert (m_di.K == y.size()+1);
@@ -155,7 +560,7 @@ bool core::apparatus<T, isParallel>::subtractDiscretes(const tMatrixDiscretes& x
 	const int ym = y.getNumRows();
 	const int yn = y.getNumCols();
 	if (xm != ym || xn != yn)
-		throw matrixException("Matrix addition not possible, boundary error");
+		throw matrixException("Matrix subtraction not possible, boundary error");
 	out.resize(m_di.K+1, tMatrixDiscrete(xm, xn, 0));
 	for (int k = 0; k <= m_di.K; ++k)
 	{
@@ -164,15 +569,16 @@ bool core::apparatus<T, isParallel>::subtractDiscretes(const tMatrixDiscretes& x
 	return true;
 }
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::multDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
+template <class T, int algo>
+bool core::apparatus<T, algo>::multDiscretes(const tMatrixDiscretes& x, const tMatrixDiscretes& y, tMatrixDiscretes& out)
 {
-	assert (m_di.K == x.size()+1);
-	assert (m_di.K == y.size()+1);
-	const int xm = x.getNumRows();
-	const int xn = x.getNumCols();
-	const int ym = y.getNumRows();
-	const int yn = y.getNumCols();
+	assert (m_di.K+1 == x.size());
+	assert (m_di.K+1 == y.size());
+	assert (m_di.K >= 0);
+	const int xm = x[0].getNumRows();
+	const int xn = x[0].getNumCols();
+	const int ym = y[0].getNumRows();
+	const int yn = y[0].getNumCols();
 	if (xn != ym)
 		throw matrixException("Matrix multiplication not possible, boundary error");
 	out.resize(m_di.K+1, tMatrixDiscrete(xm, yn, 0));
@@ -180,14 +586,14 @@ bool core::apparatus<T, isParallel>::multDiscretes(const tMatrixDiscretes& x, co
 	{
 		for (int l = 0; l <= k; ++l)
 		{
-			out[k] += x[k]*y[k-l];
+			out[k] += x[l]*y[k-l];
 		}
 	}
 	return true;
 }
 
-template <class T, bool isParallel>
-bool core::apparatus<T, isParallel>::restoreTaylorSingle(const tMatrixDiscretes& theDiscretes, tFuncMatrix& out)
+template <class T, int algo>
+bool core::apparatus<T, algo>::restoreTaylorSingle(const tMatrixDiscretes& theDiscretes, tFuncMatrix& out)
 {
 	assert (m_di.K+1 == theDiscretes.size());
 	assert (m_di.K >= 0);
--- test/test_core/src/matrix_inverse.cpp
+++ test/test_core/src/matrix_inverse.cpp
@@ -1,132 +1,146 @@
+#include "arithmetic.h"
+#include "function.h"
 #include "matrix.h"
+#include "parser.h"
+#include "apparatus.h"
 #include <iostream>
 #include <fstream>
-#include <algorithm>
-#include <iterator>
+#include <cassert>
 #include <vector>
+#include <iterator>
 
-typedef core::matrix<double> tMatrix;
-typedef std::vector<tMatrix> tDiscrete;
+typedef core::apparatus<double> tApp;
+typedef typename tApp::tMatrixDiscrete tDiscrete;
+typedef typename tApp::tMatrixDiscretes tDiscretes;
+typedef typename tApp::tFunction tFunction;
+typedef typename tApp::tFunctionPtr tFunctionPtr;
+typedef typename tApp::tFuncMatrix tMatrix;
+typedef typename tApp::tDiffInfo tDiffInfo;
 
-void inverse(const tDiscrete& c, int m, int n, int r, int K, tDiscrete& c1)
+void inverse(tApp& app, const tDiscretes& c, int m, int n, int r, int K, tDiscretes& c1)
 {
-	tDiscrete L(K, tMatrix(r, n, 0));
-	tDiscrete U(K, tMatrix(m, r, 0));
+	tDiscretes L;
+	tDiscretes U;
 
-	// First
-	for (int k = 0; k < K; ++k)
-		for (int i = 1; i <= r; ++i)
-			L[k][i][1] = c[k][i][1];
-	
-	for (int k = 0; k < K; ++k)
-	{
-		for (int j = 1; j <= r ; ++j)
-		{
-			double sum = 0;
-			for (int l = 1; l <= k; ++l)
-			{
-				sum += U[k-l][1][j]*L[l][1][1];
-			}
-			U[k][1][j] = (c[k][1][j]-sum)/L[0][1][1];
-		}
-	}
+	//app.getLU(c, r, L, U);
+	app.getInverse(c, r, c1);
 
-	// Next
-	for (int k = 0; k < K; ++k)
-	{
-		for (int i = 2; i <= r; ++i)
-		{
-			for (int p = 2; p <= i; ++p)
-			{
-				double sum = 0;
-				for (int j = 1; j <= p-1; ++j)
-				{
-					for (int l = 0; l <= k; ++l)
-					{
-						sum += L[l][i][j]*U[k-l][j][p];
-					}
-				}
-				L[k][i][p] = c[k][i][p] - sum;
-			}
-		}
-	}
+	//tDiscrete L(K, tMatrix(r, n, 0));
+	//tDiscrete U(K, tMatrix(m, r, 0));
+	//// First
+	//for (int k = 0; k < K; ++k)
+	//	for (int i = 1; i <= r; ++i)
+	//		L[k][i][1] = c[k][i][1];
+	//
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int j = 1; j <= r ; ++j)
+	//	{
+	//		double sum = 0;
+	//		for (int l = 1; l <= k; ++l)
+	//		{
+	//			sum += U[k-l][1][j]*L[l][1][1];
+	//		}
+	//		U[k][1][j] = (c[k][1][j]-sum)/L[0][1][1];
+	//	}
+	//}
 
-	for (int k = 0; k < K; ++k)
-	{
-		for (int i = 2; i <= r; ++i)
-		{
-			for (int p = i; p <= r; ++p)
-			{
-				double sum1 = 0;
-				for (int j = 1; j <= i-1; ++j)
-					for (int g = 0; g <= k; ++g)
-						sum1 += L[g][i][j]*U[k-g][j][p];
+	//// Next
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int i = 2; i <= r; ++i)
+	//	{
+	//		for (int p = 2; p <= i; ++p)
+	//		{
+	//			double sum = 0;
+	//			for (int j = 1; j <= p-1; ++j)
+	//			{
+	//				for (int l = 0; l <= k; ++l)
+	//				{
+	//					sum += L[l][i][j]*U[k-l][j][p];
+	//				}
+	//			}
+	//			L[k][i][p] = c[k][i][p] - sum;
+	//		}
+	//	}
+	//}
 
-				double sum2 = 0;
-				for (int l = 1; l <= k; ++l)
-				{
-					sum2 += U[k-l][i][p]*L[l][i][i];
-				}
-				U[k][i][p] = (c[k][i][p] - sum1 - sum2)/L[0][i][i];
-			}
-		}
-	}
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int i = 2; i <= r; ++i)
+	//	{
+	//		for (int p = i; p <= r; ++p)
+	//		{
+	//			double sum1 = 0;
+	//			for (int j = 1; j <= i-1; ++j)
+	//				for (int g = 0; g <= k; ++g)
+	//					sum1 += L[g][i][j]*U[k-g][j][p];
+
+	//			double sum2 = 0;
+	//			for (int l = 1; l <= k; ++l)
+	//			{
+	//				sum2 += U[k-l][i][p]*L[l][i][i];
+	//			}
+	//			U[k][i][p] = (c[k][i][p] - sum1 - sum2)/L[0][i][i];
+	//		}
+	//	}
+	//}
 	//std::cout << "L:\n";
 	//std::copy(L.begin(), L.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
 	//std::cout << "U:\n";
 	//std::copy(U.begin(), U.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
 
 	// Now the inverses
-	tDiscrete L1(K, tMatrix(n, r, 0));
-	tDiscrete U1(K, tMatrix(r, m, 0));
-	tDiscrete e(K, tMatrix(m, n, 0));
-	for (int i = 1; i <= m; ++i)
-		e[0][i][i] = 1;
-	
-	// Now L1
-	for (int k = 0; k < K; ++k)
-	{
-		for (int i = 1; i <= r; ++i)
-		{
-			for (int j = 1; j <= r; ++j)
-			{
-				double sum1 = 0;
-				for (int p = 1; p <= i-1; ++p)
-					for (int l = 0; l <= k; ++l)
-						sum1 += L[l][i][p]*L1[k-l][p][j];
+	//tDiscretes L1(K, tDiscrete(n, r, 0));
+	//tDiscretes U1(K, tDiscrete(r, m, 0));
+	//tDiscretes e(K, tDiscrete(m, n, 0));
+	//for (int i = 1; i <= m; ++i)
+	//	e[0][i][i] = 1;
+	//
+	//// Now L1
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int i = 1; i <= r; ++i)
+	//	{
+	//		for (int j = 1; j <= r; ++j)
+	//		{
+	//			double sum1 = 0;
+	//			for (int p = 1; p <= i-1; ++p)
+	//				for (int l = 0; l <= k; ++l)
+	//					sum1 += L[l][i][p]*L1[k-l][p][j];
 
-				double sum2 = 0;
-				for (int l = 1; l <= k; ++l)
-					sum2 += L1[k-l][i][j]*L[l][i][i];
-				L1[k][i][j] = (e[k][i][j] - sum1 - sum2)/L[0][i][i];
-			}
-		}
-	}
+	//			double sum2 = 0;
+	//			for (int l = 1; l <= k; ++l)
+	//				sum2 += L1[k-l][i][j]*L[l][i][i];
+	//			L1[k][i][j] = (e[k][i][j] - sum1 - sum2)/L[0][i][i];
+	//		}
+	//	}
+	//}
 
-	// Now U1
-	for (int k = 0; k < K; ++k)
-	{
-		for (int i = r; i >= 1; --i)
-		{
-			for (int j = 1; j <= r; ++j)
-			{
-				double sum = 0;
-				for (int p = i+1; p <= m; ++p)
-					for (int g = 0; g <= k; ++g)
-						sum += U[g][i][p]*U1[k-g][p][j];
-				U1[k][i][j] = e[k][i][j] - sum;
-			}
-		}
-	}
-	//std::cout << "L1:\n";
-	//std::copy(L1.begin(), L1.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
-	//std::cout << "U1:\n";
-	//std::copy(U1.begin(), U1.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
-	for (int k = 0; k < K; ++k)
-	{
-		for (int l = 0; l <= k; ++l)
-			c1[k] += U1[l]*L1[k-l];
-	}
+	//// Now U1
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int i = r; i >= 1; --i)
+	//	{
+	//		for (int j = 1; j <= r; ++j)
+	//		{
+	//			double sum = 0;
+	//			for (int p = i+1; p <= m; ++p)
+	//				for (int g = 0; g <= k; ++g)
+	//					sum += U[g][i][p]*U1[k-g][p][j];
+	//			U1[k][i][j] = e[k][i][j] - sum;
+	//		}
+	//	}
+	//}
+	////std::cout << "L1:\n";
+	////std::copy(L1.begin(), L1.end(), std::ostream_iterator<tDiscrete>(std::cout, "\n"));
+	////std::cout << "U1:\n";
+	////std::copy(U1.begin(), U1.end(), std::ostream_iterator<tDiscrete>(std::cout, "\n"));
+	//for (int k = 0; k < K; ++k)
+	//{
+	//	for (int l = 0; l <= k; ++l)
+	//		c1[k] += U1[l]*L1[k-l];
+	//}
 }
 
 int main(int argc, char* argv[])
@@ -142,7 +156,7 @@ int main(int argc, char* argv[])
 	}
 	std::ifstream fstr(argv[1]);
 	fstr >> m >> n >> r >> K;
-	tDiscrete c(K, tMatrix(m, n, 0));
+	tDiscretes c(K, tDiscrete(m, n, 0));
 	for (int k = 0; k < K; ++k)
 	{
 		for (int i = 1; i <= m; ++i)
@@ -150,10 +164,12 @@ int main(int argc, char* argv[])
 				fstr >> c[k][i][j];
 	}
 	std::cout << "Original:\n";
-	std::copy(c.begin(), c.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
-	tDiscrete c1(K, tMatrix(m, n, 0));
-	inverse(c, m, n, r, K, c1);
+	std::copy(c.begin(), c.end(), std::ostream_iterator<tDiscrete>(std::cout, "\n"));
+	tDiscretes c1(K, tDiscrete(m, n, 0));
+	tDiffInfo di(0, 1, K);
+	tApp app(di);
+	inverse(app, c, m, n, r, K, c1);
 	std::cout << "Inverse:\n";
-	std::copy(c1.begin(), c1.end(), std::ostream_iterator<tMatrix>(std::cout, "\n"));
+	std::copy(c1.begin(), c1.end(), std::ostream_iterator<tDiscrete>(std::cout, "\n"));
 	return 0;
 }
